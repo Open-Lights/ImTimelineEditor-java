a9f0f90b9bdce5d85fa7f09b0709d712fb63fd86
QPCrummer <66036033+QPCrummer@users.noreply.github.com>
2024-07-11 23:46:08 -0400

    Initial commit (literally nothing)

---
+++ src/main/java/com/github/qpcrummer/Main.java
@@ -0,0 +1,14 @@
+package com.github.qpcrummer;
+
+import imgui.app.Application;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(new Main());
+    }
+
+    @Override
+    public void process() {
+
+    }
+}
